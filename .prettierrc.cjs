64a90e7d170d39c7b045a5b2cd57256783b451db
zzzp <zzzp9527@163.com>
2025-07-26 16:27:45 +0800

    chore: update prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  // 行尾分号
+  semi: true,
+  // 单引号
+  singleQuote: false,
+  // 缩进
+  tabWidth: 2,
+  // 行宽
+  printWidth: 120,
+  // 末尾逗号
+  trailingComma: 'es5',
+  // 括号首尾空格
+  bracketSpacing: true,
+  // 箭头函数参数
+  arrowParens: 'avoid',
+  // 行尾换行
+  endOfLine: 'lf',
+};
